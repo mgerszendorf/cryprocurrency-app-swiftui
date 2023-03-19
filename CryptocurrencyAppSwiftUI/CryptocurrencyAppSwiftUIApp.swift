2a0548c5887fa1833b797a5a940a9f2c2915ead0
marek_gerszendorf <marek.gerszendorf@wp.pl>
2023-03-19 17:43:03 +0100

    Initial Commit

---
+++ CryptocurrencyAppSwiftUI/CryptocurrencyAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptocurrencyAppSwiftUIApp.swift
+//  CryptocurrencyAppSwiftUI
+//
+//  Created by Marek Gerszendorf on 19/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptocurrencyAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
